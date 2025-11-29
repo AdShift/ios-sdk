c61a0228f050a294d967a9036e47be0dbbbc1a06
AdShift CI <ci@adshift.com>
2025-11-29 00:05:15 +0000

    Release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to the AdShift iOS SDK will be documented in this file.
 
+## [1.0.0] - 2025-11-29
+
+### Changes
+- Update to Xcode 16.1 for CI builds
+
+---
+
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -14,13 +12,10 @@ let package = Package(
             targets: ["AdshiftSDK"])
     ],
     targets: [
-        // Binary distribution - XCFramework will be added here on first release
-        // Example:
-        // .binaryTarget(
-        //     name: "AdshiftSDK",
-        //     url: "https://github.com/AdShift/ios-sdk/releases/download/v1.0.0/AdshiftSDK.xcframework.zip",
-        //     checksum: "abc123def456..."
-        // )
+        .binaryTarget(
+            name: "AdshiftSDK",
+            url: "https://github.com/AdShift/ios-sdk/releases/download/v${VERSION}/AdshiftSDK.xcframework.zip",
+            checksum: "${CHECKSUM}"
+        )
     ]
 )
-
